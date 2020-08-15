197fe3d6f94a15de47f1a0b8ebdc3103c25d7645
hzhero1 <38153901+hzhero1@users.noreply.github.com>
2020-08-15 17:37:14 +0800

    plot

--- plot_learning_curve.py
+++ plot_learning_curve.py
@@ -1,43 +1,60 @@
 import matplotlib.pyplot as plt
 import pickle
+import numpy as np
 
 # %matplotlib inline
 # %config InlineBackend.figure_format = 'retina'
 
-loss_train_dir = "loss/10_epochs_64_loss_train_celeba.txt"
-loss_val_dir = "loss/10_epochs_64_loss_val_celeba.txt"
 
+# with open("accuracy/1_acc_train_celeba.txt", "rb") as f:
+#     accs_train = pickle.load(f)
+# with open("accuracy/1_acc_train_celeba_tra.txt", "rb") as f:
+#     accs_train_tra = pickle.load(f)
+# with open("accuracy/1_acc_train_celeba_dcgan.txt", "rb") as f:
+#     accs_train_dcgan = pickle.load(f)
+# with open("accuracy/1_acc_train_celeba_cdcgan.txt", "rb") as f:
+#     accs_train_cdcgan = pickle.load(f)
+# with open("accuracy/1_acc_train_celeba_cyclegan.txt", "rb") as f:
+#     accs_train_cyclegan = pickle.load(f)
+# with open("accuracy/1_acc_train_celeba_stargan.txt", "rb") as f:
+#     accs_train_stargan = pickle.load(f)
 
-# with open("loss/10_epochs_64_loss_train_celeba.txt", "rb") as f:
-#     losses_val = pickle.load(f)
-# with open("loss/10_epochs_64_loss_train_celeba_tra.txt", "rb") as f:
-#     losses_val_tra = pickle.load(f)
-# with open("loss/10_epochs_64_loss_train_celeba_dcgan.txt", "rb") as f:
-#     losses_val_dcgan = pickle.load(f)
-# with open("loss/10_epochs_64_loss_train_celeba_cdcgan.txt", "rb") as f:
-#     losses_val_cdcgan = pickle.load(f)
-# with open("loss/10_epochs_64_loss_train_celeba_cyclegan.txt", "rb") as f:
-#     losses_val_cyclegan = pickle.load(f)
-# with open("loss/10_epochs_64_loss_train_celeba_stargan.txt", "rb") as f:
-#     losses_val_stargan = pickle.load(f)
-
-with open("accuracy/64_acc_train_celeba_stargan.txt", "rb") as f:
-    accs_train_stargan = pickle.load(f)
-with open("accuracy/64_acc_val_celeba_stargan.txt", "rb") as f:
+with open("accuracy/2_acc_val_celeba.txt", "rb") as f:
+    accs_val = pickle.load(f)
+with open("accuracy/2_acc_val_celeba_tra.txt", "rb") as f:
+    accs_val_tra = pickle.load(f)
+with open("accuracy/2_acc_val_celeba_dcgan.txt", "rb") as f:
+    accs_val_dcgan = pickle.load(f)
+with open("accuracy/2_acc_val_celeba_cdcgan.txt", "rb") as f:
+    accs_val_cdcgan = pickle.load(f)
+with open("accuracy/2_acc_val_celeba_cyclegan.txt", "rb") as f:
+    accs_val_cyclegan = pickle.load(f)
+with open("accuracy/2_acc_val_celeba_stargan.txt", "rb") as f:
     accs_val_stargan = pickle.load(f)
 
-plt.style.use('ggplot')
-# plt.plot(losses_val, label='No augmentation')
-# plt.plot(losses_val_tra, label='Geometric')
-# plt.plot(losses_val_dcgan, label='DCGAN')
-# plt.plot(losses_val_cdcgan, label='cDCGAN')
-# plt.plot(losses_val_cyclegan, label='CycleGAN')
-# plt.plot(losses_val_stargan, label='StarGAN')
+# with open("accuracy/64_acc_train_celeba_stargan.txt", "rb") as f:
+#     accs_train_stargan = pickle.load(f)
+# with open("accuracy/64_acc_val_celeba_stargan.txt", "rb") as f:
+#     accs_val_stargan = pickle.load(f)
 
-plt.plot(accs_train_stargan, label='CycleGAN')
+plt.style.use('ggplot')
+# plt.plot(accs_train, label='No augmentation')
+# plt.plot(accs_train_tra, label='Geometric')
+# plt.plot(accs_train_dcgan, label='DCGAN')
+# plt.plot(accs_train_cdcgan, label='cDCGAN')
+# plt.plot(accs_train_cyclegan, label='CycleGAN')
+# plt.plot(accs_train_stargan, label='StarGAN')
+
+plt.plot(accs_val, label='No augmentation')
+plt.plot(accs_val_tra, label='Geometric')
+plt.plot(accs_val_dcgan, label='DCGAN')
+plt.plot(accs_val_cdcgan, label='cDCGAN')
+plt.plot(accs_val_cyclegan, label='CycleGAN')
 plt.plot(accs_val_stargan, label='StarGAN')
 
+
+plt.xticks(np.arange(0, 20), np.arange(1, 21))
 plt.xlabel('Epochs')
-plt.title('Learning curve')
+plt.title('Validation accuracy during training')
 plt.legend()
-plt.savefig('plot_64_10_epochs.png')
+plt.savefig('group3.png')
--- test.py
+++ test.py
@@ -1,5 +1,15 @@
+import argparse
+import os
+import random
 import torch
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from IPython.display import HTML
 
 # t = torch.tensor([[1, 2, 3], [2, 3, 4], [4, 4, 5]])
 # t = t.float()
@@ -20,5 +30,26 @@ import numpy as np
 # # print('{:20}{}'.format('1', 2))
 # # print()
 
-y_ = (torch.rand(5, 1) * 2).type(torch.LongTensor).squeeze()
-print(y_)
+# y_ = (torch.rand(5, 1) * 2).type(torch.LongTensor).squeeze()
+# print(y_)
+
+# Create the dataset
+dataset = dset.ImageFolder(root="../cdcgan_samples/gray_sample",
+                           transform=transforms.Compose([
+                               # transforms.Resize((128, 128)),
+                               transforms.ToTensor(),
+                           ]))
+# Create the dataloader
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=9,
+                                         shuffle=True)
+#
+# # Decide which device we want to run on
+# device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+
+# Plot some training images
+real_batch = next(iter(dataloader))
+plt.figure(figsize=(8, 8))
+plt.axis("off")
+plt.title("Training Images")
+plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(0)[:9], nrow=3, padding=2).cpu(), (1, 2, 0)))
+plt.show()
